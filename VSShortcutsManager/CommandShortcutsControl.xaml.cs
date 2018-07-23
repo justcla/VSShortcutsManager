2b1dc743cf6aa0b1dfb51aa4d430c5875e974145
Jivko Kolev <jivkok@microsoft.com>
2018-07-23 12:29:02 -0700

    All shortcuts grid plus sample data

--- VSShortcutsManager/CommandShortcutsControl.xaml.cs
+++ VSShortcutsManager/CommandShortcutsControl.xaml.cs
@@ -24,5 +24,25 @@ namespace VSShortcutsManager
         {
             InitializeComponent();
         }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var items = new List<VSShortcut>();
+            items.Add(new VSShortcut { Command = "Best Action", Shortcut = "Ctrl-A", Scope = "Global" });
+            items.Add(new VSShortcut { Command = "Second Best Action", Shortcut = "Ctrl-A", Scope = "Some Scope" });
+            items.Add(new VSShortcut { Command = "Another Best Action", Shortcut = "Ctrl-Z", Scope = "Global" });
+            items.Add(new VSShortcut { Command = "Great Action", Shortcut = "Ctrl-Shit-A", Scope = "Global" });
+            items.Add(new VSShortcut { Command = "Another Great Action", Shortcut = "Ctrl-Shit-Z", Scope = "Global" });
+
+            var grid = sender as DataGrid;
+            grid.ItemsSource = items;
+        }
+    }
+
+    class VSShortcut
+    {
+        public string Command { get; set; }
+        public string Shortcut { get; set; }
+        public string Scope { get; set; }
     }
 }
