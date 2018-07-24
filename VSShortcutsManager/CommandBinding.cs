4005fe35127cecc6172a6bccb977f1cf06285d0e
Ryan Molden <rmolden@microsoft.com>
2018-07-23 18:11:30 -0700

    Add command query engine with AllCommands property (more filters/properties/methods can be built off this).
    Work remaining: Lots of commands missing textual names, will fix them up tomorrow.

---
+++ VSShortcutsManager/CommandBinding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace VSShortcutsManager
+{
+    [DebuggerDisplay("Scope = {Scope.Name}")]
+    internal class CommandBinding
+    {
+        internal CommandId Command { get; private set;}
+
+        internal KeybindingScope Scope { get; private set; }
+
+        internal IEnumerable<BindingSequence> Chords { get; private set; }
+
+        internal CommandBinding(CommandId command, KeybindingScope scope, BindingSequence sequence)
+        {
+            this.Command = command;
+            this.Scope = scope;
+
+            this.Chords = new List<BindingSequence>() { sequence };
+        }
+
+        internal CommandBinding(CommandId command, KeybindingScope scope, BindingSequence sequence1, BindingSequence sequence2) : this(command, scope, sequence1)
+        {
+            ((List<BindingSequence>)this.Chords).Add(sequence2);
+        }
+    }
+}
---
+++ VSShortcutsManager/KeyBindingScope.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VSShortcutsManager
+{
+    [DebuggerDisplay("{Name}")]
+    public class KeybindingScope
+    {
+        internal KeybindingScope(string name, Guid guid, bool allowNavKeyBinding)
+        {
+            this.Name = name;
+            this.Guid = guid;
+            this.AllowNavKeyBinding = allowNavKeyBinding;
+        }
+
+        internal string Name { get; private set; }
+
+        internal Guid Guid { get; private set; }
+
+        internal bool AllowNavKeyBinding { get; private set; }
+    }
+
+}
