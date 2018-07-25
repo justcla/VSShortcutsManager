ec46def62e7dfb2d8848bd9b5646722d99c1ce21
Josh Stevens <joshs@microsoft.com>
2018-07-25 14:07:56 -0700

    ignore Win, Apps, and sometimes Tab; get current modifier keys

--- AddKeyboardShortcut/KeyCapturingTextBox.cs
+++ AddKeyboardShortcut/KeyCapturingTextBox.cs
@@ -1,53 +1,73 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Input;
 using VSShortcutsManager;
 
 namespace AddKeyboardShortcut
 {
-    class KeyCapturingTextBox : TextBox, INotifyPropertyChanged
+    class KeyCapturingTextBox : TextBox
     {
-        private ModifierKeys currentModifierKeys;
-        private readonly ObservableCollection<BindingSequence> bindingSequences = new ObservableCollection<BindingSequence>();
+        private readonly Tuple<Key, ModifierKeys>[] modifierKeyMapping = new[]
+        {
+            Tuple.Create(Key.LeftShift, ModifierKeys.Shift),
+            Tuple.Create(Key.RightShift, ModifierKeys.Shift),
 
-        public event PropertyChangedEventHandler PropertyChanged;
+            Tuple.Create(Key.LeftAlt, ModifierKeys.Alt),
+            Tuple.Create(Key.RightAlt, ModifierKeys.Alt),
+
+            Tuple.Create(Key.LeftCtrl, ModifierKeys.Control),
+            Tuple.Create(Key.RightCtrl, ModifierKeys.Control),
+        };
 
         public KeyCapturingTextBox()
         {
             base.PreviewKeyDown += KeyCapturingTextBox_PreviewKeyDown;
-            base.PreviewKeyUp += KeyCapturingTextBox_PreviewKeyUp;
-        }
-
-        private void KeyCapturingTextBox_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            this.currentModifierKeys &= ~GetModifierKeys(GetKey(e));
         }
 
         private void KeyCapturingTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             Key key = GetKey(e);
-            ModifierKeys modifier = GetModifierKeys(key);
-            this.currentModifierKeys |= modifier;
-            bool isModifier = (modifier != ModifierKeys.None);
-            if (!isModifier)
+
+            // Certain keys are not captured by design.  Check for those here and exit early.
+            if (!ShouldCapture(key))
             {
-                e.Handled = true;
-
-                if (bindingSequences.Count == 2)
-                {
-                    bindingSequences.Clear();
-                }
-
-                string keyName = key.ToString();
-                bindingSequences.Add(new BindingSequence(this.currentModifierKeys, keyName));
-
-                base.Text = string.Join(", ", bindingSequences);
-                base.CaretIndex = base.Text.Length;
+                return;
             }
+
+            // Allow user to input Tab only when there are no keys in the sequence yet; otherwise, Tab
+            // moves to the next control.
+            if (BindingSequences.Count > 0 && (key == Key.Tab))
+            {
+                return;
+            }
+
+            e.Handled = true;
+
+            if (BindingSequences.Count == 2)
+            {
+                BindingSequences.Clear();
+            }
+
+            ModifierKeys modifiers = GetCurrentModifierKeys();
+            string keyName = key.ToString();
+            BindingSequences.Add(new BindingSequence(modifiers, keyName));
+
+            base.Text = string.Join(", ", BindingSequences);
+            base.CaretIndex = base.Text.Length;
         }
 
-        public ObservableCollection<BindingSequence> BindingSequences => this.bindingSequences;
+        private bool ShouldCapture(Key key)
+        {
+            return
+                !modifierKeyMapping.Select(m => m.Item1).Contains(key)
+                && key != Key.Apps
+                && key != Key.LWin
+                && key != Key.RWin;
+        }
+
+        public ObservableCollection<BindingSequence> BindingSequences { get; } = new ObservableCollection<BindingSequence>();
 
         private Key GetKey(KeyEventArgs args)
         {
@@ -67,10 +87,23 @@ namespace AddKeyboardShortcut
                 case Key.RightCtrl: return ModifierKeys.Control;
                 case Key.LeftShift: return ModifierKeys.Shift;
                 case Key.RightShift: return ModifierKeys.Shift;
-                case Key.LWin: return ModifierKeys.Windows;
-                case Key.RWin: return ModifierKeys.Windows;
                 default: return ModifierKeys.None;
             }
+        }
+
+        private ModifierKeys GetCurrentModifierKeys()
+        {
+            ModifierKeys result = ModifierKeys.None;
+
+            foreach (Tuple<Key, ModifierKeys> map in modifierKeyMapping)
+            {
+                if (Keyboard.IsKeyDown(map.Item1))
+                {
+                    result |= map.Item2;
+                }
+            }
+
+            return result;
         }
     }
 }
