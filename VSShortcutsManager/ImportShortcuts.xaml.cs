1cbbb896ebcb18f45e50449d85ed9a9459c743ca
gandatandya <ganda.tandya@gmail.com>
2018-07-25 10:41:58 -0700

    Able to import VS settings, diplay shortcuts, load only the desired shortcuts through temporary settings file (#9)
    * Parsing VSSettings XML file
    
    * Add UI and checkbox is working
    
    * Add OK Button to and able to filter out checked shortcuts
    
    * Combine VSShortcut object
    
    * Load XML based on what user chooses in the window and add cancel button
    
    * Enable key 'Esc' to cancel import
    
    * Delete Jivko's code that has been removed from master

---
+++ VSShortcutsManager/ImportShortcuts.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VSShortcutsManager
+{
+    /// <summary>
+    /// Interaction logic for ImportShortcuts.xaml
+    /// </summary>
+    public partial class ImportShortcuts : DialogWindow
+    {
+        public bool isCancelled { get; private set; }
+
+        public ImportShortcuts(List<VSShortcut> shortcuts)
+        {
+            var shortcutsUI = new List<VSShortcutUI>();
+            foreach (var shortcut in shortcuts)
+            {
+                shortcutsUI.Add(new VSShortcutUI(true, shortcut));
+            }
+            this.DataContext = shortcutsUI;
+            InitializeComponent();
+        }
+
+        public class VSShortcutUI : INotifyPropertyChanged
+        {
+            public bool isIncluded { private set; get; }
+            public VSShortcut vsShortcut { get; }
+
+            public bool Included
+            {
+                get
+                {
+                    return this.isIncluded;
+                }
+                set
+                {
+                    this.isIncluded = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string Command
+            {
+                get
+                {
+                    return this.vsShortcut.Command;
+                }
+            }
+
+            public string Scope
+            {
+                get
+                {
+                    return this.vsShortcut.Scope;
+                }
+            }
+
+            public string Shortcut
+            {
+                get
+                {
+                    return this.vsShortcut.Shortcut;
+                }
+            }
+
+            public string Conflict
+            {
+                get
+                {
+                    return this.vsShortcut.Conflict;
+                }
+            }
+
+            public VSShortcutUI(bool included, VSShortcut shortcut)
+            {
+                this.isIncluded = included;
+                this.vsShortcut = shortcut;
+            }
+
+            private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
+        private void OkButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.isCancelled = false;
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.isCancelled = true;
+            Close();
+        }
+
+        public List<VSShortcut> GetUncheckedShortcuts()
+        {
+            var shortcuts = new List<VSShortcut>();
+            foreach (var shortcut in (List<VSShortcutUI>) (this.DataContext))
+            {
+                if (!shortcut.isIncluded)
+                {
+                    shortcuts.Add(shortcut.vsShortcut);
+                }
+            }
+            return shortcuts;
+        }
+    }
+}
