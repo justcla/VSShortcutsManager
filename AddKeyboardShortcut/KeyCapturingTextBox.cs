4d9e9b4183da4789fa014c411ddc16a619e04f05
JoshuaBStevens <joshs@microsoft.com>
2018-07-25 14:29:05 -0700

    updates to shortcut textbox (#19)
    * make a TextBox that captures all keystrokes
    
    * ignore Win, Apps, and sometimes Tab; get current modifier keys
    
    * simplify
    
    * add tabbing hint below shorcut input box

--- AddKeyboardShortcut/KeyCapturingTextBox.cs
+++ AddKeyboardShortcut/KeyCapturingTextBox.cs
@@ -1,53 +1,64 @@
 ﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Windows.Controls;
 using System.Windows.Input;
 using VSShortcutsManager;
 
 namespace AddKeyboardShortcut
 {
-    class KeyCapturingTextBox : TextBox, INotifyPropertyChanged
+    class KeyCapturingTextBox : TextBox
     {
-        private ModifierKeys currentModifierKeys;
-        private readonly ObservableCollection<BindingSequence> bindingSequences = new ObservableCollection<BindingSequence>();
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public KeyCapturingTextBox()
         {
             base.PreviewKeyDown += KeyCapturingTextBox_PreviewKeyDown;
-            base.PreviewKeyUp += KeyCapturingTextBox_PreviewKeyUp;
-        }
-
-        private void KeyCapturingTextBox_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            this.currentModifierKeys &= ~GetModifierKeys(GetKey(e));
         }
 
         private void KeyCapturingTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             Key key = GetKey(e);
-            ModifierKeys modifier = GetModifierKeys(key);
-            this.currentModifierKeys |= modifier;
-            bool isModifier = (modifier != ModifierKeys.None);
-            if (!isModifier)
+
+            // Certain keys are not captured by design.  Check for those here and exit early.
+            if (!ShouldCapture(key))
             {
-                e.Handled = true;
-
-                if (bindingSequences.Count == 2)
-                {
-                    bindingSequences.Clear();
-                }
-
-                string keyName = key.ToString();
-                bindingSequences.Add(new BindingSequence(this.currentModifierKeys, keyName));
-
-                base.Text = string.Join(", ", bindingSequences);
-                base.CaretIndex = base.Text.Length;
+                return;
             }
+
+            // Allow user to input Tab only when there are no keys in the sequence yet; otherwise, Tab
+            // moves to the next control.
+            if (BindingSequences.Count > 0 && (key == Key.Tab))
+            {
+                return;
+            }
+
+            e.Handled = true;
+
+            if (BindingSequences.Count == 2)
+            {
+                BindingSequences.Clear();
+            }
+
+            ModifierKeys modifiers = Keyboard.Modifiers;
+            string keyName = key.ToString();
+            BindingSequences.Add(new BindingSequence(modifiers, keyName));
+
+            base.Text = string.Join(", ", BindingSequences);
+            base.CaretIndex = base.Text.Length;
         }
 
-        public ObservableCollection<BindingSequence> BindingSequences => this.bindingSequences;
+        private bool ShouldCapture(Key key)
+        {
+            return
+                key != Key.Apps
+                && key != Key.LWin
+                && key != Key.RWin
+                && key != Key.LeftAlt
+                && key != Key.RightAlt
+                && key != Key.LeftCtrl
+                && key != Key.RightCtrl
+                && key != Key.LeftShift
+                && key != Key.RightShift;
+        }
+
+        public ObservableCollection<BindingSequence> BindingSequences { get; } = new ObservableCollection<BindingSequence>();
 
         private Key GetKey(KeyEventArgs args)
         {
@@ -55,22 +66,6 @@ namespace AddKeyboardShortcut
                 (args.Key == Key.System)
                 ? args.SystemKey
                 : args.Key;
-        }
-
-        private ModifierKeys GetModifierKeys(Key key)
-        {
-            switch (key)
-            {
-                case Key.LeftAlt: return ModifierKeys.Alt;
-                case Key.RightAlt: return ModifierKeys.Alt;
-                case Key.LeftCtrl: return ModifierKeys.Control;
-                case Key.RightCtrl: return ModifierKeys.Control;
-                case Key.LeftShift: return ModifierKeys.Shift;
-                case Key.RightShift: return ModifierKeys.Shift;
-                case Key.LWin: return ModifierKeys.Windows;
-                case Key.RWin: return ModifierKeys.Windows;
-                default: return ModifierKeys.None;
-            }
         }
     }
 }
